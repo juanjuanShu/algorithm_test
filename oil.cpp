36491ff28beca02eed5e476379865c60c205ec42
juanjuanShu <juanShu168@163.com>
2021-01-03 14:50:36 +0800

    微调，修改返回值，添加注释

--- oil.cpp
+++ oil.cpp
@@ -11,23 +11,25 @@ using namespace std;
 * 一开始不理解 k 个加油站怎么会有 k + 1个数据
 * 距离     1   2   3   4  5  1  6  6
 * 加油站 $   1   2   3  4  5  6  7   ^
-* 
+*
 * 先逐个扫描各个加油站的距离，若发现其中有一个距离大于汽车加满油能跑的距离，则输出 “无解”
 * 否则尽可能的跑远（贪心策略）
-* 
+*
 * 每停一次，则加满油，num++
 */
 
 int oil(vector<double> arr_distance, int distance_after_oil) {
 	int num = 0;
 
-	for (int i = 0,s = 0; i < arr_distance.size(); i++) {
+	for (int i = 0, s = 0; i < arr_distance.size(); i++) {
 		//无解
 		if (distance_after_oil < arr_distance[i]) {
 			cout << "No solution" << endl;
-			return num;
+			return 0;
 		}
 
+		//到达一个站，已经走的距离（耗油） + 这个站到下一站的距离（耗油），加满油能否支持到下一站
+		//如果能，那么停的次数加1，并且 s = 当前站到下一站的距离（到下一站前要耗的油）
 		s += arr_distance[i];
 		if (s > distance_after_oil) {
 			num++;
@@ -45,18 +47,18 @@ int main() {
 	vector<double> arr_distance;
 
 	cout << "输入加满油后最多能行驶的距离：" << endl;
-	cin >> distance_after_oil ;
+	cin >> distance_after_oil;
 	cout << "输入加油站的个数:" << endl;
 	cin >> oil_station_nums;
-	
 
-	cout << "输入第 k 个加油站与第 k - 1个加油站之间的距离"<<endl;
+
+	cout << "输入第 k 个加油站与第 k - 1个加油站之间的距离" << endl;
 	for (int i = 0; i <= oil_station_nums; i++) {
 		cin >> tmp;
 		arr_distance.push_back(tmp);
 	}
 
-	int num = oil(arr_distance,distance_after_oil);
+	int num = oil(arr_distance, distance_after_oil);
 	cout << "最少加油次数" << num << endl;
 	return 0;
 }
